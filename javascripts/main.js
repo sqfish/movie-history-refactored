58dcf0e590ac5b5ab7d4b3eef2cafed6bdf5f01c
Rachel Morgan <rachyllmorgan@gmail.com>
2015-08-11 14:59:22 -0500

    ajax pulling object

--- javascripts/main.js
+++ javascripts/main.js
@@ -20,38 +20,25 @@ requirejs(
 		
 		var myFirebaseRef = new Firebase("https://movie-history-cpr.firebaseio.com/");
 		
-		myFirebaseRef.on("value", function(snapshot) {
-  var movies = snapshot.val();
-		});
 		
 		
-	// Get OMDB API movie info
-//		
-//			$.ajax({
-//        url: "http://www.omdbapi.com/?",
-//			method: "GET",
-//			data: JSON
-//      }).done(function(getMovie) {
-//				console.log(getMovie);
-//			});
-//	}	);
-
-var $Form = $('form'), $Container = $('#container');
-$Container.hide();
-$Form.on('submit', function(p_oEvent){
-    var sUrl, sMovie, oData;
-    p_oEvent.preventDefault();
-sMovie = $Form.find('input').val();
-    sUrl = 'http://www.omdbapi.com/?t=' + sMovie + '&type=movie&tomatoes=true'
-    $.ajax(sUrl, {
-        complete: function(p_oXHR, p_sStatus){
-            oData = $.parseJSON(p_oXHR.responseText);
-            console.log(oData);
-            $Container.find('.title').text(oData.Title);
-            $Container.find('.plot').text(oData.Plot);
-            $Container.find('.poster').html('<img src="' + oData.Poster + '"/>');
-            $Container.find('.year').text(oData.Year);
-            $Container.show();
-        }
-    });    
-});
+// Get OMDB API movie info
+		
+	function getMovie(title) {
+    		$.ajax({
+    url: "http://www.omdbapi.com/?",
+    data: {
+        s: title,
+    },
+    success: function(data) {
+        console.log(data);
+					}
+    });
+	};
+		
+		$(".subTitle").on("click", function(){
+			var title = $("#movieName").val();
+			console.log("title", title);
+			console.log(getMovie(title));
+    });
+			});
