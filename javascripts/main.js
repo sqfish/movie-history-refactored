ad70e449b6727dbbc431f60459d40d23fe5e55de
Caitlin Stein <caitlin.teter@gmail.com>
2015-08-11 14:26:16 -0500

    switch button works

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,10 +4,12 @@ requirejs.config({
     'jquery': '../bower_components/jquery/dist/jquery.min',
 		'firebase': '../bower_components/firebase/firebase',
     'hbs': '../bower_components/require-handlebars-plugin/hbs',
-    'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min'
+    'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min',
+    'bootstrap-switch': '../bower_components/bootstrap-switch/dist/js/bootstrap-switch.min'
   },
   shim: {
     'bootstrap': ['jquery'],
+    'bootstrap-switch': ['bootstrap'],
 		'firebase': {
 			exports: 'Firebase'
 		}
@@ -15,43 +17,41 @@ requirejs.config({
 });
 
 requirejs(
-["jquery", "firebase", "hbs", "bootstrap"],
-	function ($, _firebase, Handlebars, bootstrap) {
+["jquery", "firebase", "hbs", "bootstrap", "addMovies", "bootstrap-switch"],
+	function ($, _firebase, Handlebars, bootstrap, addMovies, bootstrapSwitch) {
 		
 		var myFirebaseRef = new Firebase("https://movie-history-cpr.firebaseio.com/");
 		
 		myFirebaseRef.on("value", function(snapshot) {
-  var movies = snapshot.val();
-		});
-		
-		
-	// Get OMDB API movie info
-//		
-//			$.ajax({
-//        url: "http://www.omdbapi.com/?",
-//			method: "GET",
-//			data: JSON
-//      }).done(function(getMovie) {
-//				console.log(getMovie);
-//			});
-//	}	);
+      var movies = snapshot.val();
+      loadMovies(movies);
+    });
 
-var $Form = $('form'), $Container = $('#container');
-$Container.hide();
-$Form.on('submit', function(p_oEvent){
-    var sUrl, sMovie, oData;
-    p_oEvent.preventDefault();
-sMovie = $Form.find('input').val();
-    sUrl = 'http://www.omdbapi.com/?t=' + sMovie + '&type=movie&tomatoes=true'
-    $.ajax(sUrl, {
-        complete: function(p_oXHR, p_sStatus){
-            oData = $.parseJSON(p_oXHR.responseText);
-            console.log(oData);
-            $Container.find('.title').text(oData.Title);
-            $Container.find('.plot').text(oData.Plot);
-            $Container.find('.poster').html('<img src="' + oData.Poster + '"/>');
-            $Container.find('.year').text(oData.Year);
-            $Container.show();
-        }
-    });    
-});
+    function loadMovies(data) {
+      require(['hbs!../templates/movie-list'], function(template) {
+        $("#movie-list").append(template(data));
+        $("[name='viewed']").bootstrapSwitch();
+        console.log("loadMovies function called");
+      });
+
+    }
+
+		
+  // $("#addMusicButton").on("click", function(){
+  //   var musicData = {};
+  //   //grab values from form and store in object
+  //     musicData = {
+  //       "title": $("#addTitle").val(),
+  //       "year": $("#addArtist").val(),
+  //       "actors": $("#addAlbum").val(),
+  //       "rating":
+  //       "viewed":
+  //     };
+
+  //     musicData = JSON.stringify(musicData);
+  //     console.log("stringified musicData", musicData);
+  //     addMusic.addMusic(musicData);
+  //     $("#addTitle").val("");
+  //     $("#addArtist").val("");
+  //     $("#addAlbum").val("");
+ });
