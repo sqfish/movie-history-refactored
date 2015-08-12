c3b5c725ad859314981ede9d6ea56477fdb67849
Caitlin Stein <caitlin.teter@gmail.com>
2015-08-12 11:51:42 -0500

    delete button is functional

--- javascripts/deleteButton.js
+++ javascripts/deleteButton.js
@@ -3,19 +3,11 @@ define(["firebase"],function(_firebase) {
 
  return {
    delete: function(argument) {
-     var ref = new Firebase("https://movie-history-cpr.firebaseio.com/movies" + argument);
+     var ref = new Firebase("https://movie-history-cpr.firebaseio.com/movies/" + argument);
      ref.remove();
+     console.log("ref", argument);
    }
  };
 
 }); 
 
- $(document).on("click", '.delete', function() {
-   var deleteTitle = $(this).siblings('h2').text();
-   var titleKey = '';
-   titleKey = _.findKey(allSongObject, function(song) {
-     return song.title.replace(/ /g,"") === deleteTitle.replace(/ /g, "");
-   });
-   console.log(titleKey);
-   delete.delete(titleKey);
- });  
